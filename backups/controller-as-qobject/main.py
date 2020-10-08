3bca081a93e97ebcd9699fa851144b8ebb9ce760
Howard Wang <dr.howardwang@gmail.com>
2020-10-08 07:29:10 -0700

    BRUH

--- backups/controller-as-qobject/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from Controller import Controller
-
-if __name__ == '__main__':
-    appctxt = ApplicationContext()
-    controller = Controller(sys.argv)
-    exit_code = appctxt.app.exec_()
-    sys.exit(exit_code)
